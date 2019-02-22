de6e4d46d7a080d9f36dd0a5ce25efa99462eaad
Alvin <alvingonzales1045@gmail.com>
2019-02-22 09:58:09 +0800

    y

--- src/app/services-webapi/Queue_Num/queue-num.service.ts
+++ src/app/services-webapi/Queue_Num/queue-num.service.ts
@@ -6,23 +6,24 @@ import { queuedata } from './queuedata';
 @Injectable({
   providedIn: 'root'
 })
+
 export class QueueNumService {
-  // // web server string
-  // private _url: string = "http://192.168.100.16:59511/api/QueueNumber";
-
-  // // eto yung mismong fetch ng db
-  // getQueueNames(): Observable<queuedata[]> {
-  //   return this.http.get<queuedata[]>(this._url);
-  // }
-
-  //instantiate ng http client
-  constructor(private http: HttpClient) { }
-
   // web server string
-  private _url: string = "../../../assets/sampleAPI.json";
+  private _url: string = "https://api.openweathermap.org/data/2.5/weather?lat=14.32675975&lon=120.90618458021615&appid=ef006e1141bb597afc50c0f9278f33e5";
 
   // eto yung mismong fetch ng db
   getQueueNames(): Observable<queuedata[]> {
     return this.http.get<queuedata[]>(this._url);
   }
+
+  //instantiate ng http client
+  constructor(private http: HttpClient) { }
+
+  // // web server string
+  // private _url: string = "../../../assets/sampleAPI.json";
+
+  // // eto yung mismong fetch ng db
+  // getQueueNames(): Observable<queuedata[]> {
+  //   return this.http.get<queuedata[]>(this._url);
+  // }
 }
--- src/app/services-webapi/Queue_Num/queuedata.ts
+++ src/app/services-webapi/Queue_Num/queuedata.ts
@@ -1,7 +1,53 @@
 export interface queuedata {
-    Id: number,
-    Queue_Num: number,
-    Date: Date
+    coord: Coord;
+    weather: Weather[];
+    base: string;
+    main: Main;
+    visibility: number;
+    wind: Wind;
+    clouds: Clouds;
+    dt: number;
+    sys: Sys;
+    id: number;
+    name: string;
+    cod: number;
 }
 
-//structure nung api na irereceive
+export interface Sys {
+    type: number;
+    id: number;
+    message: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+}
+
+export interface Clouds {
+    all: number;
+}
+
+export interface Wind {
+    speed: number;
+    deg: number;
+}
+
+export interface Main {
+    temp: number;
+    pressure: number;
+    humidity: number;
+    temp_min: number;
+    temp_max: number;
+}
+
+export interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+}
+
+export interface Coord {
+    lon: number;
+    lat: number;
+}
+//structure nung api na irereceive, ayusin mo in a way na iisang model na lang lagi yung tatawagin tas puro export export na lang hihe
