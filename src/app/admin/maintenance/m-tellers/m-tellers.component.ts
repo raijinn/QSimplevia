d42b823b7501bc6ab31576c450a2b885ec8b2d96
Parraluman <egjdp331@gmail.com>
2019-03-18 17:05:04 +0800

    ma nameis jeff login/teller services/tellers component

--- src/app/admin/maintenance/m-tellers/m-tellers.component.ts
+++ src/app/admin/maintenance/m-tellers/m-tellers.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { PostTeller } from 'src/app/models/queueing_models';
+import { PostTellerService } from '../../../services/services-webapi/Post/post-teller.service';
+import { GetTellerService } from '../../../services/services-webapi/GetTeller/get-teller.service';
 
 @Component({
   selector: 'app-m-tellers',
   templateUrl: './m-tellers.component.html',
   styleUrls: ['./m-tellers.component.css']
 })
-export class MTellersComponent implements OnInit {
+export class MTellersComponent implements OnInit{
 
-  constructor() { }
+  public anyTeller: any [];
+
+  public passTeller = new PostTeller('','','','','','');
+  
+  
+  constructor(private _GetTellerServices: GetTellerService, private _PostTellerServices: PostTellerService){ } 
 
   ngOnInit() {
+    // GET
+    this._GetTellerServices.getTellerData()
+      .subscribe(data => this.anyTeller = data);
   }
 
+  onSubmit() {
+    this._PostTellerServices.addTeller(this.passTeller)
+      .subscribe(
+        data => this._GetTellerServices.getTellerData()
+          .subscribe(data => this.anyTeller = data),
+        error => console.error('Error!', error)
+      );
+  }
 }
+
+
